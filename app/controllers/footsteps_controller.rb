527002a40867a3d8c2db8588a6949c48b66e2ead
Kent Yoneda <yone.1130.kent@icloud.com>
2022-11-02 21:32:01 +0900

    歩数記録に関するモデルコントローラーを入れた。ホーム画面作成途中。変更加えました。1

--- app/controllers/footsteps_controller.rb
+++ app/controllers/footsteps_controller.rb
@@ -9,7 +9,6 @@ class FootstepsController < ApplicationController
 
   def create
     @footstep = Footstep.new(footstep_params)
-    # @my_thread.user_id = current_user.id if current_user
     if @footstep.save
       redirect_to footsteps_path
     else
--- db/migrate/20221005101935_create_footsteps.rb
+++ db/migrate/20221005101935_create_footsteps.rb
@@ -1,8 +1,8 @@
 class CreateFootsteps < ActiveRecord::Migration[7.0]
   def change
     create_table :footsteps do |t|
-      t.integer :footstep
-      t.date :date
+      t.integer :footstep, :null => false
+      t.date :date , :null => false
       t.integer :favo
 
       t.timestamps
